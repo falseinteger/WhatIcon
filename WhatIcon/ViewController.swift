431436829a33cd8614efd5345d5f80b8fc23e0e6
Falseinteger <falseinteger@gmail.com>
2016-04-22 04:51:57 +0300

    Initial Commit

---
+++ WhatIcon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatIcon
+//
+//  Created by Дмитрий Порошков on 22.04.16.
+//  Copyright © 2016 Dmitriy Poroshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
